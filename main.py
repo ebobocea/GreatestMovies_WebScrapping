e664e8cfeb4e9d24e967283917587e9b7df51b6c
Elisei <elisey93@icloud.com>
2022-10-10 15:25:56 +0100

    completed

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ article_texts = []
 for article_tag in articles:
     text = article_tag.getText()
     article_texts.append(text)
-print(article_texts)
+article_texts.reverse()
+
+with open('FilmsToWatch.txt', 'w', encoding="utf-8") as f:
+    for article in article_texts:
+        f.write(f"{article}\n")
 
 
